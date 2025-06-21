f47c2ea3a3eda2034fae411eb2d2b152ff14a934
thedouglasaraujo <thedouglasaraujo@gmail.com>
2025-06-21 15:17:22 -0300

    feat(atoms): cria componente CourseActionsButtons

---
+++ frontend/src/components/atoms/CourseActionsButtons/index.js
@@ -0,0 +1,15 @@
+import { Delete, Edit } from '@mui/icons-material';
+import { Button, Stack } from '@mui/material';
+
+export default function CourseActionsButtons({ onEdit, onDelete }) {
+  return (
+    <Stack direction="row" spacing={1}>
+      <Button variant="outlined" color="primary" startIcon={<Edit />} onClick={onEdit}>
+        Editar
+      </Button>
+      <Button variant="outlined" color="error" startIcon={<Delete />} onClick={onDelete}>
+        Excluir
+      </Button>
+    </Stack>
+  );
+}

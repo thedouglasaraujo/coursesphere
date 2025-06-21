b82d416907559cb00e0ae847ce6d6596879f1040
thedouglasaraujo <thedouglasaraujo@gmail.com>
2025-06-21 13:41:46 -0300

    feat(layouts): cria componente ProtectedLayout

---
+++ frontend/src/layouts/ProtectedLayout.js
@@ -0,0 +1,20 @@
+import { Box } from '@mui/material';
+import { Outlet } from 'react-router-dom';
+import TopBar from '~/components/organisms/TopBar';
+
+export default function ProtectedLayout() {
+    return (
+        <>
+            <TopBar />
+            <Box
+                sx={{
+                    width: '100%',
+                    padding: 0,
+                    boxSizing: 'border-box',
+                }}
+            >
+                <Outlet />
+            </Box>
+        </>
+    );
+}

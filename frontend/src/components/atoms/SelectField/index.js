956f8dc6a37a7fdfe492634d3d8202564f3cf52d
thedouglasaraujo <thedouglasaraujo@gmail.com>
2025-06-21 00:57:05 -0300

    feat(atoms): cria componente SelectField

---
+++ frontend/src/components/atoms/SelectField/index.js
@@ -0,0 +1,35 @@
+import {
+  FormControl,
+  FormHelperText,
+  InputLabel,
+  MenuItem,
+  Select,
+  Typography,
+} from '@mui/material';
+
+export default function SelectField({
+  label,
+  name,
+  register,
+  rules,
+  errors,
+  options,
+  placeholder,
+}) {
+  return (
+    <>
+      <Typography variant="subtitle1">{label}</Typography>
+      <FormControl fullWidth margin="normal" error={!!errors[name]}>
+        <InputLabel>{placeholder}</InputLabel>
+        <Select {...register(name, rules)} label={label}>
+          {options.map(option => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </Select>
+        <FormHelperText>{errors[name]?.message}</FormHelperText>
+      </FormControl>
+    </>
+  );
+}

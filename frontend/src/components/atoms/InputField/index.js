6e37f3a09df158bceee6d6186c3d6d3e4549d928
thedouglasaraujo <thedouglasaraujo@gmail.com>
2025-06-20 23:35:02 -0300

    feat(atoms): cria componente InputField

---
+++ frontend/src/components/atoms/InputField/index.js
@@ -0,0 +1,29 @@
+import { TextField, Typography } from '@mui/material';
+
+export default function InputField({
+    label,
+    register,
+    name,
+    rules,
+    errors,
+    type = 'text',
+    sxLabel,
+    placeholder,
+}) {
+    return (
+        <>
+            <Typography variant="subtitle1" sx={sxLabel}>
+                {label}
+            </Typography>
+            <TextField
+                placeholder={placeholder}
+                fullWidth
+                margin="normal"
+                type={type}
+                {...register(name, rules)}
+                error={!!errors[name]}
+                helperText={errors[name]?.message}
+            />
+        </>
+    );
+}

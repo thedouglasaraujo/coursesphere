ae99d034cf9f455c8c88449c79036b365701d8f7
thedouglasaraujo <thedouglasaraujo@gmail.com>
2025-06-21 13:51:07 -0300

    feat(routes): adiciona configuração básica de rotas

---
+++ frontend/src/routes/Routes.js
@@ -0,0 +1,33 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import ProtectedLayout from '~/layouts/ProtectedLayout';
+import Login from '~/pages/Login';
+
+const isAuthenticated = () => {
+    return !!localStorage.getItem('token');
+};
+
+function PrivateRoute({ children }) {
+    return isAuthenticated() ? children : <Navigate to="/login" replace />;
+}
+
+export default function AppRoutes() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+
+                <Route
+                    path="/"
+                    element={
+                        <PrivateRoute>
+                            <ProtectedLayout />
+                        </PrivateRoute>
+                    }
+                >
+                </Route>
+
+                <Route path="*" element={<Navigate to="/login" replace />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
